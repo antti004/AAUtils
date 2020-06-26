87d97ecb9450b6c1b43926caaef114280616602a
Antti <antti.arrak@hotmail.com>
2020-06-26 13:28:19 +0300

    Updates.

---
+++ StringUtilities/ArrayHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AAUtilities.StringUtilities
+{
+    public static class ArrayHelper
+    {
+        /// <summary>
+        /// Remove empty items from array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static string[] Trim(this string[] array) => array.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+    }
+}
---
+++ StringUtilities/Parsers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AAUtilities.StringUtilities
+{
+    public static class Parsers
+    {
+        public static string Letters(this string input) => Regex.Replace(input, "[^a-zA-Z]", "");
+        public static string Numbers(this string input) => Regex.Replace(input, "[^0-9]", "");
+
+
+        /// <summary>
+        /// Remove non-numeric chars from string.
+        /// Example "57.200 KG" -> 57.200
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string StipNonNumeric(this string input) => new string(input.Where(c => (Char.IsDigit(c) || c == '.' || c == ',')).ToArray());
+    }
+}
